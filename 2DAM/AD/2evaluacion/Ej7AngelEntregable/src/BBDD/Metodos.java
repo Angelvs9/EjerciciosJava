9e4e6e2ae30d215dbe8edabd049cb42b2bee28f0
Angelvs9 <angelvazquezsaura@gmail.com>
2025-01-14 22:24:55 +0100

    casi casi
    falta que no puede tener mas de uno foto, aun no se porque cuando le vuelvo a dar el anterior pierde su foto

--- 2DAM/AD/2evaluacion/Ej7AngelEntregable/src/BBDD/Metodos.java
+++ 2DAM/AD/2evaluacion/Ej7AngelEntregable/src/BBDD/Metodos.java
@@ -1,14 +1,17 @@
 package BBDD;
 
+
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
@@ -141,29 +144,52 @@ public class Metodos {
     
     public static boolean subirFoto(Connection gestor,int id,String foto){
         boolean correcto=false;
-        String insert = "INSERT INTO bfoto (ncodigo, foto) VALUES (?, ?)";
+        String consulta = "UPDATE clientes SET bfoto = ? WHERE ncodigo = ?";
         use(gestor);
         try {
-
-            ByteArrayOutputStream byteArray=new ByteArrayOutputStream();
-            ObjectOutputStream oos=new ObjectOutputStream(byteArray);
-            oos.writeObject(foto);
-            PreparedStatement ps=gestor.prepareStatement(insert);
-            ps.setInt(1, id); 
-            ps.setBytes(2, fotoBytes); 
+            File f=new File(foto);
+            FileInputStream fis=new FileInputStream(f);
+            PreparedStatement ps=gestor.prepareStatement(consulta);
+            ps.setBinaryStream(1, fis);
+            ps.setInt(2, id);
             ps.executeUpdate();
-            
+            ps.close();
+            fis.close();
+            correcto=true;
             
         } catch (IOException ex) {
             Logger.getLogger(Metodos.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
             Logger.getLogger(Metodos.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        
+
         return correcto;
     }
     
-    
+    public static void descargarFoto(Connection gestor,int id){
+        use(gestor);
+        try {
+            //el file es el nombre que le pondrás al archivo que quieres coger
+            File f=new File("archivoResultante.jpg");
+            PreparedStatement ps=gestor.prepareStatement("SELECT bfoto FROM clientes WHERE ncodigo = ?");
+            ps.setInt(1, id);
+            ResultSet rs=ps.executeQuery();
+            FileOutputStream fos = new FileOutputStream(f);
+            if(rs.next()){
+                Blob blob =rs.getBlob("bfoto");
+                byte[] data=blob.getBytes(1, (int)blob.length());
+                fos.write(data);
+            }
+            ps.close();
+            rs.close();
+            fos.close();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Metodos.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Metodos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
     
 }
--- 2DAM/AD/2evaluacion/Ej7AngelEntregable/src/Vista/main.java
+++ 2DAM/AD/2evaluacion/Ej7AngelEntregable/src/Vista/main.java
@@ -1,9 +1,57 @@
+
 package Vista;
 
+import BBDD.Metodos;
+import BBDD.gestorConexion;
+import static BBDD.Metodos.crearBD;
+import static BBDD.Metodos.subirFoto;
+import static BBDD.Metodos.descargarFoto;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author Angel
  */
 public class main {
+
+
+    public static void main(String[] args) {
+    gestorConexion gestor=new gestorConexion();
+        try {
+            
+            crearBD(gestor.getConn(),"BBDD.sql");
+            System.out.println("bd creada");
+            Statement stmt = gestor.getConn().createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT MAX(ncodigo) FROM clientes");
+            int n=0;
+            if (rs.next()) {
+                n=rs.getInt(1);
+            }
+            rs.close();
+            stmt.close();
+            
+            Scanner sc=new Scanner(System.in);
+            System.out.println("pon un numero del 1 al "+n+" (a excepcion del 2)");
+            int temp=sc.nextInt();
+            sc.nextLine();
+            System.out.println("pon el nombre de la foto que quieres subir: ");
+            String foto=sc.nextLine();
+            System.out.println("subiendo la foto proporcionada a la persona con este codigo: (con extensión)"+temp);
+            System.out.println("subida foto al cliente con codigo: "+temp+" ---> "+ subirFoto(gestor.getConn(), temp, foto));
+            System.out.println("descargando foto...");
+            descargarFoto(gestor.getConn(), temp);
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        gestor.cerrar_Conexion();
+    }
     
 }
