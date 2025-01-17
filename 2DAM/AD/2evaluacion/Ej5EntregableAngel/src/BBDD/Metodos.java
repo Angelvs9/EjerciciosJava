c79cc4da8002e72023a484262a0808e0a083af58
Angelvs9 <angelvazquezsaura@gmail.com>
2025-01-17 12:19:13 +0100

    en proceso
    nif,nombre,apellidos,cp,cc de clientes pasarlo a datos_fiscales

--- 2DAM/AD/2evaluacion/Ej5EntregableAngel/src/BBDD/Metodos.java
+++ 2DAM/AD/2evaluacion/Ej5EntregableAngel/src/BBDD/Metodos.java
@@ -12,6 +12,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
@@ -25,37 +26,71 @@ public class Metodos {
     
     public static boolean crearBaseDatos(Connection gestor,String bd){
         boolean temp=false;
-        String linea="";
-        String consulta="";
-        File f=new File(bd);
-        try {
-            BufferedReader br=new BufferedReader(new FileReader(f));
-            Statement sta=gestor.createStatement();
-            while((linea=br.readLine())!=null){
-                if (linea.isEmpty() && linea.startsWith("--")) {
-                    consulta+=linea;
-                    if (consulta.endsWith(";")) {
-                        sta.execute(consulta);
-                        consulta="";
-                        temp=true;
+        if (!ExisteBD(gestor, bd)) {
+            String linea="";
+            String consulta="";
+            File f=new File(bd);
+            try {
+                BufferedReader br=new BufferedReader(new FileReader(f));
+                Statement sta=gestor.createStatement();
+                while((linea=br.readLine())!=null){
+                    if (!linea.isEmpty() && !linea.startsWith("--")) {
+                        consulta+=linea;
+                        if (consulta.endsWith(";")) {
+                            sta.execute(consulta);
+                            consulta="";
+                            temp=true;
+                        }
                     }
+
                 }
-                
+                br.close();
+                sta.close();
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(Ej5EntregableAngel.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(Ej5EntregableAngel.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (SQLException ex) {
+                Logger.getLogger(Metodos.class.getName()).log(Level.SEVERE, null, ex);
             }
-            br.close();
-            sta.close();
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(Ej5EntregableAngel.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(Ej5EntregableAngel.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
-            Logger.getLogger(Metodos.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        }//cierre if
+        
         
         return temp;
     }
     
+    private static boolean ExisteBD(Connection gestor,String nombre){
+        boolean existe=false;
+        try {
+            Statement sta=gestor.createStatement();
+            gestor.createStatement(); 
+            String consulta = "SELECT COUNT(*) AS total FROM information_schema.schemata WHERE schema_name = '" + nombre + "'";
+            ResultSet rs=sta.executeQuery(consulta);
+            if (rs.next()) {
+                //si hay mas de 0 bd con ese nombre ya existe
+                if(rs.getInt("total")>0){
+                    existe=true;
+                    System.out.println("a ver ");
+                }
+            }
+            rs.close();
+            sta.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Metodos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("no funciona de momento "+existe);
+        return existe;
+    }
     
+    
+    public static boolean traspasoDatos(Connection bdPostgres,Connection bdMySql){
+        boolean traspaso=false;
+        
+        
+        
+        
+        return traspaso;
+    }
     
     
 }
---
+++ 2DAM/AD/2evaluacion/Ej5EntregableAngel/src/Modelo/Cuenta_cliente.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Angel
+ */
+public class Cuenta_cliente {
+    
+    private long codigo;//primary key esta bien lo de long
+    private String tipo;
+    private double saldo;
+
+    public Cuenta_cliente(long codigo, String tipo, double saldo) {
+        this.codigo = codigo;
+        this.tipo = tipo;
+        this.saldo = saldo;
+    }
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Cuenta_cliente{" + "codigo=" + codigo + ", tipo=" + tipo + ", saldo=" + saldo + '}';
+    }
+    
+}
